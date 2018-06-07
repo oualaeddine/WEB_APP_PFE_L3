f3246363bb777c5152cf838775dc88233e9ccb64
berrehal ouala eddine <berrehaloualaeddine@gmail.com>
2018-06-07 23:34:00 +0100

    commit hekak bark

--- src/control/ivs/IVSConsts.java
+++ src/control/ivs/IVSConsts.java
@@ -7,7 +7,7 @@ public class IVSConsts {
     public static final String FOURCHETTE_PRIX_MENU_SERVLET_URL = "/ivs_FOURCHETTE_PRIX",
             REGION_MENU_SERVLET_URL = "/ivs_REGION_MENU",
             REGION_MENU_HANDELER_SERVLET_URL = "/ivs_REGION_MENU_HANDELER",
-            TYPE_MENU_SERVLET_URL = "/ivs_",
+            TYPE_MENU_SERVLET_URL = "/ivs_TYPE_MENU",
             ROOMS_MENU_HANDELER_SERVLET_URL = "/ivs_ ROOMS_MENU_HANDELER",
             SUPERFICIE_MENU_HANDELER_SERVLET_URL = "/ivs_SUPERFICIE_MENU_HANDELER",
             RECAP_SERVLET_URL = "/ivs_RECAP",
@@ -20,7 +20,8 @@ public class IVSConsts {
             CALL_OPERATOR_SERVLET_URL = "/ivs_CALL_OPERATOR",
             PROGRAM_VISIT_SERVLET_URL = "/ivs_PROGRAM_VISIT",
             CHANGE_LANGUAGE_SERVLET_URL = "/ivs_CHANGE_LANGUAGE";
-
+    public static final String WELCOME_HANDELER_SERVLET_URL = "/ivs_WELCOME_HANDELER",
+            WELCOME_MENU_SERVLET_URL = "/ivs_WELCOME_MENU";
     public static final String WELCOME_MP3_URL = "";
 
     public static final String FR_PROGRAMMER_VISITE_MP3_URL = "",
@@ -41,8 +42,7 @@ public class IVSConsts {
             YOU_CHOOSED_ARABIC_MP3_URL = "",
             AR_STANDARD_WELCOME_MESSAGE_MP3_URL = "",
             AR_MAIN_MENU_MP3_URL = "";
-    public static final String WELCOME_HANDELER_SERVLET_URL = "",
-            WELCOME_MENU_SERVLET_URL = "";
+
     private static final String AR_NO_LOGEMENTS_MP3_URL = "",
             FR_NO_LOGEMENTS_MP3_URL = "";
 
--- src/control/servlets/MyServlet.java
+++ src/control/servlets/MyServlet.java
@@ -1,6 +1,5 @@
 package control.servlets;
 
-import model.beans.humans.Person;
 import utils.MyConsts;
 
 import javax.servlet.ServletException;
@@ -42,9 +41,7 @@ public class MyServlet extends HttpServlet {
 
         try {
             this.getServletContext().getRequestDispatcher("/home").forward(request, response);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ServletException e) {
+        } catch (IOException | ServletException e) {
             e.printStackTrace();
         }
 
@@ -63,11 +60,11 @@ public class MyServlet extends HttpServlet {
         response.sendRedirect(MyConsts.LOGIN_SERVLET_URL + "?error=" + wrongCredentialsError);
     }
 
-    protected void redirectToDashboard(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    protected void redirectToDashboard(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.sendRedirect("/DashboardServlet");
     }
 
-    protected void redirectToDashboard(HttpServletRequest request, HttpServletResponse response, int error) throws IOException, ServletException {
+    protected void redirectToDashboard(HttpServletRequest request, HttpServletResponse response, int error) throws IOException {
         if (error == 0) {
             response.sendRedirect("/DashboardServlet");
         } else {

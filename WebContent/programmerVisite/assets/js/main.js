60ebf4fdc79ccb7c76cc531b17a122d19de21296
berrehal ouala eddine <berrehaloualaeddine@gmail.com>
2018-06-08 10:49:07 +0100

    ajouter vente ta3 l'operateur tmchi

--- WebContent/programmerVisite/assets/js/main.js
+++ WebContent/programmerVisite/assets/js/main.js
@@ -1,17 +1,4 @@
-/*!
 
- =========================================================
- * Paper Bootstrap Wizard - v1.0.2
- =========================================================
-
- * Product Page: https://www.creative-tim.com/product/paper-bootstrap-wizard
- * Copyright 2017 Creative Tim (http://www.creative-tim.com)
- * Licensed under MIT (https://github.com/creativetimofficial/paper-bootstrap-wizard/blob/master/LICENSE.md)
-
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- */
 
 // Paper Bootstrap Wizard Functions
 
